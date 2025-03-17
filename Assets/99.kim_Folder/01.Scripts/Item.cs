a2df2137200bd1dc24d3f36f4872096a4c975a6d
gamble98 <krex0528@gmail.com>
2025-03-17 19:17:36 +0900

    Feat : First Backup
    Body : Replay System

---
+++ Assets/99.kim_Folder/01.Scripts/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public string imageFileName;
+    public string title;
+    public string subtitle;
+}
