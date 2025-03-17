78c8a393f93a56be3db2c31d7a190fd4ec10dbfa
jokeun <joekun00@gmail.com>
2025-03-17 17:48:07 +0900

    Feat : 상점패널 유아이 배치
    #ZJGK-10 -2

---
+++ Assets/99.joe_Folder/01.Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Joe_namespace
+{
+    public class GameManager : Singleton<GameManager>
+    {
+    
+    
+    
+    
+    
+        protected override void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+        }
+    
+    }
+}
+
+
+
+
+
---
+++ Assets/99.joe_Folder/01.Scripts/UI/PanelController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+//using DG.Tweening;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private GameObject panelObject;
+
+
+    private void Awake()
+    {
+        
+    }
+
+    
+    
+    
+    private void Start()
+    {
+        //ShowPanel();
+    }
+    
+    
+    
+    
+    
+    // private void ShowPanel()
+    // {
+    //     _backgroundImage.DOFade(0, 0);
+    //     panelObject.GetComponent<CanvasGroup>().DOFade(0, 0);
+    //     panelObject.GetComponent<RectTransform>().DOAnchorPosY(-500f, 0);
+    //     
+    //     _backgroundImage.DOFade(1f, 0.2f);
+    //     panelObject.GetComponent<CanvasGroup>().DOFade(1f, 0.2f);
+    //     panelObject.GetComponent<RectTransform>().DOAnchorPosY(0, 0.2f);
+    //     
+    // }
+    //
+    //
+    // public void OnClickCloseButton()
+    // {
+    //     HidePopupPanel();
+    // }
+    
+    
+    
+    
+    
+    
+}
