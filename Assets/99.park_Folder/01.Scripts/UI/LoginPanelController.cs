4dddc32cbf13b275de5169fe2d69ec6950797550
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-03-17 17:42:20 +0900

    Feat: 로그인-회원가입 임시 구현 ZJGK-6

---
+++ Assets/99.park_Folder/01.Scripts/UI/LoginPanelController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using park_namespace;
+using TMPro;
+using UnityEngine;
+
+public struct LoginData
+{
+    public string id;
+    public string password;
+}
+
+public class LoginPanelController : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _idInputField;
+    [SerializeField] private TMP_InputField _passwordInputField;
+
+    public void OnClickLogInButton()
+    {
+        string id = _idInputField.text;
+        string password = _passwordInputField.text;
+        if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(password))
+        {
+            return;
+        }
+
+        var loginData = new LoginData();
+        loginData.id = id;
+        loginData.password = password;
+
+        if (PlayerPrefs.GetString("ID") == id)
+        {
+            if (PlayerPrefs.GetString("Password") == password)
+            {
+                Debug.Log("로그인 성공");
+            }
+            else
+            {
+                Debug.Log("비밀번호가 틀렸습니다.");
+            }
+        }
+     
+      
+
+
+        
+        //TODO: 로그인 진심 구현 
+    }
+
+    public void OnClickSignUpButton()
+    {
+        Destroy(gameObject);
+       GameManager.Instance.OpenSignUpPanel();
+    }
+}
---
+++ Assets/99.park_Folder/01.Scripts/UI/SignUpPanelController.cs
@@ -0,0 +1,78 @@
+﻿using park_namespace;
+using TMPro;
+using UnityEngine;
+
+
+public struct SignUpData
+{
+    public string nickname;
+    public string id;
+    public string password;
+   
+}
+public class SignUpPanelController : PanelController
+{
+    [SerializeField] private TMP_InputField _idInputField;
+    [SerializeField] private TMP_InputField _nicknameInputField;
+    [SerializeField] private TMP_InputField _passwordInputField;
+    [SerializeField] private TMP_InputField _confirmPasswordInputField;
+
+    private void SaveSignUpDataToPlayerPrefs(SignUpData signUpData)
+    {
+        PlayerPrefs.SetString("NickName", signUpData.nickname);
+        PlayerPrefs.SetString("ID", signUpData.id);
+        PlayerPrefs.SetString("Password", signUpData.password);
+        PlayerPrefs.Save();
+    }
+    public void OnClickConfirmButton()
+    {
+        var id= _idInputField.text;
+        var nickname = _nicknameInputField.text;
+        var password = _passwordInputField.text;
+        var confirmPassword = _confirmPasswordInputField.text;
+        if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(nickname) ||
+            string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+        {
+            //TODO: 입력값이 비어있음을 알리는 팝업창 표시 
+            return;
+        }
+        if (string.IsNullOrEmpty(id))
+        {
+            //TODO: // ID입력하세요 팝업창
+            return;
+        }
+
+        if (password.Equals(confirmPassword))
+        {
+            SignUpData signUpData = new SignUpData
+            {
+                id = id,
+                nickname = nickname,
+                password = password
+
+            };
+            // signUpData.id = id;
+            // signUpData.nickname = nickname;
+            // signUpData.password = password;
+            SaveSignUpDataToPlayerPrefs(signUpData);
+            Debug.Log("회원가입 데이터 저장");
+            //TODO: 회원가입 진행 
+        }
+        else
+        {
+            //GameManeger.Instance.OpenConfirmPanel("비밀번호가 서로 다릅니다.", () =>
+            // {
+            //   _passwordInputField.text = "";
+            //_confirmPasswordInputField.text = "";
+            //  });
+        }
+    }
+
+    public void OnClickBackButton()
+    {
+        Debug.Log("BackButton누름!");
+        Destroy(gameObject);
+        GameManager.Instance.OpenLoginPanel();
+        //TODO: 뒤로가기 누르면 로그인 패널이 나오게 하기 
+    }
+}
