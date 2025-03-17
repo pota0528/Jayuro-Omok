653f95bed959310cd568dc0c23f2808330ebec26
waterismul <tmtoaooo@gmail.com>
2025-03-17 14:25:45 +0900

    Feat : 메세지 팝업 UI 디자인
    ZJGK-42

---
+++ Assets/99.yu_Folder/BaseUIController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace yu_namespace
+{
+    public class BaseUIController : MonoBehaviour
+    {
+        [SerializeField] RectTransform rectTransform;
+        
+    }
+}
+
