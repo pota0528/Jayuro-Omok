711fd0b99b2856851eed719f0f1456b38863b404
gamble98 <krex0528@gmail.com>
2025-03-18 10:31:49 +0900

    Feat : backup 2nd
    Body : backup

---
+++ Assets/99.kim_Folder/01.Scripts/MatchData.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System;
+
+namespace kim_namespace
+{
+    [System.Serializable]
+    public class Move
+    {
+        public int row; // 행
+        public int col; // 열
+        public string color; // "흑" 또는 "백"
+    }
+
+    [System.Serializable]
+    public class MatchData
+    {
+        public string title; // 유저 닉네임
+        public string date; // 저장 날짜
+        public List<Move> moves; // 이동 목록
+    }
+}
