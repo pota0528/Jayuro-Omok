e42d31987ea5cb466129e12c49702ed7ca6a5dcb
LEE-YJ00 <dldmlwjd22@gmail.com>
2025-03-19 23:57:55 +0900

    Feat : MCTS Algorithm_ver02
    Body : test

---
+++ Game/Assets/lee_Folder/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+namespace lee_namespace
+{
+    public class Constants
+    {
+        // 승점포인트 에 몰아서 이기던 지던
+        // 총 게임 수 
+        // .. 
+        private static int winCount = 0;
+        public static int WinCount { get; set; }
+    }
+}
