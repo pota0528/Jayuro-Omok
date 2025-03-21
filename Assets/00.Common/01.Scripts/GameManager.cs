29e7733cd2ec4de24bbba9f8facbbdf646e054b0
chanchan2 <106582155+chanchan2hi@users.noreply.github.com>
2025-03-21 13:48:49 +0900

    Feat:GameManager이랑 UIManager분리

--- Assets/00.Common/01.Scripts/GameManager.cs
+++ Assets/00.Common/01.Scripts/GameManager.cs
@@ -8,10 +8,6 @@ using UnityEngine.UI;
    
  public class GameManager : Singleton<GameManager>
     {
-        [SerializeField] private GameObject loginPanel;
-        [SerializeField] private GameObject signUpPanel;
-        [SerializeField] private GameObject userPanel;
-        [SerializeField] private GameObject profilePanel;
         [SerializeField] private BlockController _blockController;
         [SerializeField] private GameUIController _gameUIController;
         [SerializeField] private Button confirmButton;
@@ -23,17 +19,12 @@ using UnityEngine.UI;
         private PlayerType[,] _board;
         private TurnType currentTurn; // 턴 상태 업데이트 위함..
         private Canvas _canvas;
-        //추가: 옵션패널 
-        //DB관련
-        public GameObject playerPrefab;
-        private DBManager mongoDBManager;
-        //프로필 이미지 인덱스를 관리하는 변수 
-        private int currentIamgeIndex = 0; 
+       
         void Start()
         {
             StartGame();
-            OpenLoginPanel();
-            mongoDBManager = FindObjectOfType<DBManager>();
+           
+          
         }
 
         /// <summary>
@@ -232,82 +223,7 @@ using UnityEngine.UI;
 
             return moves;
         }
-
-        public void OpenLoginPanel()
-        {
-            
-            if (_canvas != null)
-            {
-                Debug.Log("로그인패널열기");
-              Instantiate(loginPanel, _canvas.transform);
-               
-            }
-        }
-
-        public void OpenSignUpPanel()
-        {
-          
-            if (_canvas != null)
-            {
-             Instantiate(signUpPanel, _canvas.transform);
-               
-            }
-        }
-
-        public void OpenUserPanel()
-        {
-            if (_canvas != null)
-            {
-                Instantiate(userPanel, _canvas.transform);
-            }
-        }
-
-        public void OpenProfilePanel()
-        {
-            if (_canvas != null)
-            {
-                Instantiate(profilePanel, _canvas.transform);
-            }
-            
-        }
-        //이미지 인덱스 설정
-        public void SetProfileImageIndex(int index)
-        {
-            currentIamgeIndex = index;
-        }
         
-        //현재 이미지 인덱스를 가져오는 메서드
-        public int GetProfileImageIndex()
-        {
-            return currentIamgeIndex;
-        }
-        // 이미지 업데이트
-        public void UpdateUserProfileImage(Sprite newProfileImage)
-        {
-            UserPanelController userPanelController = FindObjectOfType<UserPanelController>();
-            if (userPanelController != null)
-            {
-                userPanelController.UpdateProfileImage(newProfileImage);
-            }
-        }
-
-
-        public void LoginPlayer(string id, string password)
-        {
-            PlayerData playerData = mongoDBManager.Login(id, password);
-
-            if (playerData != null)
-            {
-                GameObject playerObject = Instantiate (playerPrefab,Vector3.zero,Quaternion.identity);
-                PlayerManager playerScript = playerObject.GetComponent<PlayerManager>();
-
-                if (playerObject != null)
-                {
-                    playerScript.SetPlayerData(playerData);
-                }
-            }
-        }
-
    
 
         protected override void OnSceneLoaded(Scene scene, LoadSceneMode mode)
