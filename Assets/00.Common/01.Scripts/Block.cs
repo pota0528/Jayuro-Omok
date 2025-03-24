bad79e7b13b98863f2a85b8012b3f72a696171a5
DESKTOP-19P5MFT\newpr <100wja100@gmail.com>
2025-03-24 20:49:45 +0900

    Wip : prefab 할당 완료, block은 나중에 prefab으로 교체 예정

--- Assets/00.Common/01.Scripts/Block.cs
+++ Assets/00.Common/01.Scripts/Block.cs
@@ -9,7 +9,6 @@ public class Block : MonoBehaviour
     public Sprite PreSprite;           // 미리보기 스프라이트
     public SpriteRenderer markerSpriteRenderer;  // 마커 표시용
     public SpriteRenderer previewSpriteRenderer; // 미리보기 표시용
-    public TextMeshPro markerMoveIndexText;      // 이동 인덱스 텍스트
 
     public enum MarkerType { None, Black, White, Forbidden }
     public MarkerType BlockType { get; private set; } = MarkerType.None;
